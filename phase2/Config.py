9245d464578a9dc3ee43bdcddfccc4aafa71fa27
Satrajit Maitra <satrajit16@gmail.com>
2019-10-01 17:58:45 -0700

    Formatting using black

--- phase2/Config.py
+++ phase2/Config.py
@@ -6,18 +6,18 @@ class Config(object):
         self.__json_file_path = "/Users/satrajitmaitra/HandImages/constants.json"
         with open(self.__json_file_path) as f:
             self.constants = json.load(f)
-    
+
     def database_name(self):
-        return self.constants.get('DATABASE_NAME')
-    
+        return self.constants.get("DATABASE_NAME")
+
     def collection_name(self):
-        return self.constants.get('COLLECTION_NAME')
-    
+        return self.constants.get("COLLECTION_NAME")
+
     def mongo_url(self):
-        return self.constants.get('MONGO_URL')
-    
+        return self.constants.get("MONGO_URL")
+
     def read_path(self):
-        return self.constants.get('READ_PATH')
-    
+        return self.constants.get("READ_PATH")
+
     def write_path(self):
-        return self.constants.get('WRITE_PATH')
+        return self.constants.get("WRITE_PATH")
--- phase2/Task1.py
+++ phase2/Task1.py
@@ -7,21 +7,21 @@ from latentsymantics import LatentSymantics
 
 
 def starter(feature_model, dimension_reduction, k):
-	config_object = Config()
-	path = config_object.read_path()
-	
-	files = os.listdir(path)
+    config_object = Config()
+    path = config_object.read_path()
 
-	x = []
-	for file in files:
-	    print("Reading file: {}".format(file))
-	    image = cv2.imread(path + file)
-	    
-	    feature_descriptor = Descriptor(image, feature_model).feature_descriptor
-	    x.append(feature_descriptor)
+    files = os.listdir(path)
 
-	x = np.array(x)
-	
-	latent_symantics = LatentSymantics(x, k, dimension_reduction).latent_symantics
+    x = []
+    for file in files:
+        print("Reading file: {}".format(file))
+        image = cv2.imread(path + file)
 
-	print(latent_symantics)
+        feature_descriptor = Descriptor(image, feature_model).feature_descriptor
+        x.append(feature_descriptor)
+
+    x = np.array(x)
+
+    latent_symantics = LatentSymantics(x, k, dimension_reduction).latent_symantics
+
+    print(latent_symantics)
