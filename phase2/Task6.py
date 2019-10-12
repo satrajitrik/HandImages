1a6bbea0f90b26de50f537c4df369005eb91c26e
Satrajit Maitra <satrajit16@gmail.com>
2019-10-12 16:41:51 -0700

    Adding method doc for task6.py and preprocess.py

--- phase2/Task6.py
+++ phase2/Task6.py
@@ -6,41 +6,83 @@ from latentsymantics import LatentSymantics
 from scipy.spatial import distance
 
 
-def compare(source_subject, other_subjects):
-    """
+"""
+    Idea is to extract all dorsal and palmar feature descriptors for a given subject.
+    
+    dorsal/palmar feature vectors are represented as numpy arrays of the form 
+    ((# of dorsal/palmar images for a subject) X (feature descriptor length)) 
+    where the # of rows is variable but the # of columns is constant for a given feature descriptor.
+
+    We take the transpose of these feature vector matrices to represent all dorsal/palmar images 
+    as features instead of objects for a given subject and apply dimensionality reduction 
+    to reduce the number of features to 1 feature/image which best represents the subject. 
+
+    After applying dimensionality reduction, we get 1 dorsal latent symantics matrix and 1 
+    palmar latent symantics matrix. The shape of these matrices are same. ie. (1 X (feature descriptor length))
+
+    We now concatenate the dorsal and palmar latent symantics and apply cosime similarity to
+    compare two subjects to get the result.
+
+    Parameters:
+    k = 1 (Reduced dimension)
+    choice: {
 	    1: PCA,
 	    2: SVD,
 	    3: NMF,
 	    4: LDA
-	    in LatentSymantics(_, k, 1/2/3/4)
-	"""
+    }
+
+    NOTE: Checked using Color moments with PCA. You guys can check for others.
+"""
+
+
+def compare(source_subject, other_subjects, k=1, choice=1):
     source_dorsal_latent_symantics = LatentSymantics(
-        np.transpose(source_subject["dorsal"]), 1, 1
+        np.transpose(source_subject["dorsal"]), k, choice
     ).latent_symantics
     source_palmar_latent_symantics = LatentSymantics(
-        np.transpose(source_subject["palmar"]), 1, 1
+        np.transpose(source_subject["palmar"]), k, choice
     ).latent_symantics
+
+    source_dorsal_latent_symantics = [
+        x for item in source_dorsal_latent_symantics.tolist() for x in item
+    ]
+    source_palmar_latent_symantics = [
+        x for item in source_palmar_latent_symantics.tolist() for x in item
+    ]
+    source_latent_symantics = np.concatenate(
+        (
+            np.array(source_dorsal_latent_symantics),
+            np.array(source_palmar_latent_symantics),
+        )
+    )
 
     distances = []
     for subject in other_subjects:
         if subject["gender"] == source_subject["gender"]:
             other_dorsal_latent_symantics = LatentSymantics(
-                np.transpose(subject["dorsal"]), 1, 1
+                np.transpose(subject["dorsal"]), k, choice
             ).latent_symantics
             other_palmar_latent_symantics = LatentSymantics(
-                np.transpose(subject["palmar"]), 1, 1
+                np.transpose(subject["palmar"]), k, choice
             ).latent_symantics
 
-            dorsal_distance = distance.euclidean(
-                source_dorsal_latent_symantics, other_dorsal_latent_symantics
-            )
-            palmar_distance = distance.euclidean(
-                source_palmar_latent_symantics, other_palmar_latent_symantics
+            other_dorsal_latent_symantics = [
+                x for item in other_dorsal_latent_symantics.tolist() for x in item
+            ]
+            other_palmar_latent_symantics = [
+                x for item in other_palmar_latent_symantics.tolist() for x in item
+            ]
+            other_latent_symantics = np.concatenate(
+                (
+                    np.array(other_dorsal_latent_symantics),
+                    np.array(other_palmar_latent_symantics),
+                )
             )
 
-            average_distance = float(dorsal_distance + palmar_distance) / 2
+            dist = distance.cosine(source_latent_symantics, other_latent_symantics)
 
-            distances.append([subject["subject_id"], average_distance])
+            distances.append([subject["subject_id"], dist])
 
     distances = sorted(distances, key=lambda x: x[1])
     return distances[:3]
