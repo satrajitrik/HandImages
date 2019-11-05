c18500b908a25bd3c887e29b487c02eb01e44659
Satrajit Maitra <satrajit16@gmail.com>
2019-11-05 01:01:36 -0700

    code formatting

--- phase3/imageprocessor.py
+++ phase3/imageprocessor.py
@@ -9,95 +9,88 @@ from config import Config
 
 
 class ImageProcessor(object):
-	def __init__(self):
-		self.read_path = Config().read_path()
-		self.k = 200
-		self.id_vector_pair = self.__process_files()
+    def __init__(self):
+        self.read_path = Config().read_path()
+        self.k = 200
+        self.id_vector_pair = self.__process_files()
 
+    def __process_files(self):
+        files = os.listdir(self.read_path)
 
-	def __process_files(self):
-		files = os.listdir(self.read_path)
+        ids, x = [], []
+        for file in files:
+            print("Reading file: {}".format(file))
+            image = cv2.imread("{}{}".format(self.read_path, file))
 
-		ids, x = [], []
-		for file in files:
-			print("Reading file: {}".format(file))
-			image = cv2.imread("{}{}".format(self.read_path, file))
+            feature_descriptor = self.__color_moments(image)
 
-			feature_descriptor = self.__color_moments(image)
+            ids.append(file.replace(".jpg", ""))
+            x.append(feature_descriptor)
 
-			ids.append(file.replace(".jpg", ""))
-			x.append(feature_descriptor)
+        latent_semantics = self.__svd(np.array(x), self.k)
 
-		latent_semantics = self.__svd(np.array(x), self.k)
+        return ids, latent_semantics
 
-		return ids, latent_semantics
+    def __color_moments(self, image):
+        img_out = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+        # Saving height, width, and channel  of a given image
+        y_len, x_len, channel = img_out.shape
 
+        y, u, v = cv2.split(img_out)
+        # Defining needed variables to store mean,deviation, skew of different channels of an image
 
-	def __color_moments(self, image):
-		img_out = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-		# Saving height, width, and channel  of a given image
-		y_len, x_len, channel = img_out.shape
+        meanOfY, meanofU, meanofV = [], [], []
+        sdOfY, sdofU, sdofV = [], [], []
+        skewOfY, skewofU, skewofV = [], [], []
 
-		y, u, v = cv2.split(img_out)
-		# Defining needed variables to store mean,deviation, skew of different channels of an image
+        for i in range(0, y_len, 100):
 
-		meanOfY, meanofU, meanofV = [], [], []
-		sdOfY, sdofU, sdofV = [], [], []
-		skewOfY, skewofU, skewofV = [], [], []
+            for j in range(0, x_len, 100):
+                # Slicing image into 100*100 matrix
+                # using numpy package to determine mean and deviation of sub blocks
+                meanimg = np.nanmean(
+                    img_out[i : i + 100, j : j + 100],
+                    axis=tuple(range(img_out[i : i + 100, j : j + 100].ndim - 1)),
+                )
+                deviationimg = np.std(
+                    img_out[i : i + 100, j : j + 100],
+                    axis=tuple(range(img_out[i : i + 100, j : j + 100].ndim - 1)),
+                )
 
-		for i in range(0, y_len, 100):
+                arr_y = y[i : i + 100, j : j + 100]
+                arr_u = u[i : i + 100, j : j + 100]
+                arr_v = v[i : i + 100, j : j + 100]
 
-		    for j in range(0, x_len, 100):
-		        # Slicing image into 100*100 matrix
-		        # using numpy package to determine mean and deviation of sub blocks
-		        meanimg = np.nanmean(
-		            img_out[i : i + 100, j : j + 100],
-		            axis=tuple(range(img_out[i : i + 100, j : j + 100].ndim - 1)),
-		        )
-		        deviationimg = np.std(
-		            img_out[i : i + 100, j : j + 100],
-		            axis=tuple(range(img_out[i : i + 100, j : j + 100].ndim - 1)),
-		        )
+                # appending mean of each color channel of every 100*100 sub matrix
+                meanOfY.append(meanimg[0])
+                meanofU.append(meanimg[1])
+                meanofV.append(meanimg[2])
 
-		        arr_y = y[i : i + 100, j : j + 100]
-		        arr_u = u[i : i + 100, j : j + 100]
-		        arr_v = v[i : i + 100, j : j + 100]
+                # appending standard deviation of each color channel aof every 100*100 sub matrix
+                sdOfY.append(deviationimg[0])
+                sdofU.append(deviationimg[1])
+                sdofV.append(deviationimg[2])
 
-		        # appending mean of each color channel of every 100*100 sub matrix
-		        meanOfY.append(meanimg[0])
-		        meanofU.append(meanimg[1])
-		        meanofV.append(meanimg[2])
+                # appending Skewness of each color channel aof every 100*100 sub matrix
+                min_skew = abs(
+                    np.nanmin(
+                        [
+                            skew(arr_y.flatten()),
+                            skew(arr_u.flatten()),
+                            skew(arr_v.flatten()),
+                        ]
+                    )
+                )
 
-		        # appending standard deviation of each color channel aof every 100*100 sub matrix
-		        sdOfY.append(deviationimg[0])
-		        sdofU.append(deviationimg[1])
-		        sdofV.append(deviationimg[2])
+                skewOfY.append(skew(arr_y.flatten()))
+                skewofU.append(skew(arr_u.flatten()))
+                skewofV.append(skew(arr_v.flatten()))
 
-		        # appending Skewness of each color channel aof every 100*100 sub matrix
-		        min_skew = abs(
-		            np.nanmin(
-		                [
-		                    skew(arr_y.flatten()),
-		                    skew(arr_u.flatten()),
-		                    skew(arr_v.flatten()),
-		                ]
-		            )
-		        )
+        color_moment_feature_vector = np.concatenate(
+            [meanOfY, meanofU, meanofV, sdOfY, sdofU, sdofV, skewOfY, skewofU, skewofV],
+            axis=0,
+        )
+        return color_moment_feature_vector
 
-		        skewOfY.append(skew(arr_y.flatten()))
-		        skewofU.append(skew(arr_u.flatten()))
-		        skewofV.append(skew(arr_v.flatten()))
-
-		color_moment_feature_vector = np.concatenate(
-		    [meanOfY, meanofU, meanofV, sdOfY, sdofU, sdofV, skewOfY, skewofU, skewofV],
-		    axis=0,
-		)
-		return color_moment_feature_vector
-
-
-	def __svd(self, x, k):
-		return TruncatedSVD(n_components=self.k).fit_transform(x)
-
-
-
-
+    def __svd(self, x, k):
+        return TruncatedSVD(n_components=self.k).fit_transform(x)
--- phase3/task5.py
+++ phase3/task5.py
@@ -9,10 +9,7 @@ def starter():
     # input_vector = np.array([np.random.randint(-199, 200, size=10) for _ in range(100)])
     ids, input_vector = ImageProcessor().id_vector_pair
 
-    modified_input_vector = [
-        (ids[i], input_vector[i])
-        for i in range(len(ids))
-    ]
+    modified_input_vector = [(ids[i], input_vector[i]) for i in range(len(ids))]
 
     # Displaying the 5 hashtables
     LSH(5, 5, modified_input_vector).beautify_and_print()
